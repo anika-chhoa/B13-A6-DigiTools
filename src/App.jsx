0f4d455edcb9a6e19e5b32855cbf637a5995163d
anika-chhoa <chhoa.anika07@gmail.com>
2026-03-31 05:02:45 +0600

    Product cards designed and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-
-import { Suspense } from 'react'
-import './App.css'
-import Banner from './Components/Banner'
-import Navbar from './Components/Navbar'
-import StatSection from './Components/StatSection'
-import Tab from './Components/Tab'
-import Cards from './Components/Cards'
+import { Suspense } from "react";
+import "./App.css";
+import Banner from "./Components/Banner";
+import Cards from "./Components/Cards";
+import Navbar from "./Components/Navbar";
+import StatSection from "./Components/StatSection";
+import Tab from "./Components/Tab";
 
 function App() {
-  const fetchData= async ()=>{
-    const res=await fetch("/Tools.json");
-    return res.json()
-  }
-  const dataPromise=fetchData();
+  const fetchData = async () => {
+    const res = await fetch("/Tools.json");
+    return res.json();
+  };
+  const dataPromise = fetchData();
 
   return (
     <>
-      <Navbar/>
-      <Banner/>
-      <StatSection/>
-      <Tab/>
+      <Navbar />
+      <Banner />
+      <StatSection />
+      <Tab />
       <Suspense>
-        <Cards dataPromise={dataPromise}/>
+        <Cards dataPromise={dataPromise} />
       </Suspense>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,124 +1,71 @@
 const Card = ({ product }) => {
+  const tagStyles = {
+    Popular: {
+      bg: "bg-[#e1e7ffFF]",
+      text: "text-[#4F39F6]",
+    },
+    "Best Seller": {
+      bg: "bg-[#fef3c6FF]",
+      text: "text-[#bb4d00FF]",
+    },
+    New: {
+      bg: "bg-[#dbfce7FF]",
+      text: "text-[#0a883eFF]",
+    },
+  };
   return (
     <div>
-      <div className="card max-w-96 mx-auto bg-base-100 rounded-2xl relative shadow-sm">
-        <span className="badge badge-xs badge-warning absolute top-2 right-2">
-          Most Popular
-        </span>
-        <div><span>{product.icon}</span></div>
-        <div className="card-body">
-          <div className="flex justify-between">
-            <h2 className="text-3xl font-bold">Premium</h2>
-            <span className="text-xl">$100/mo</span>
-          </div>
-          <ul className="mt-6 flex flex-col gap-2 text-xs">
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>High-resolution image generation</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>Customizable style templates</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>Batch processing capabilities</span>
-            </li>
-            <li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-success"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span>AI-driven image enhancements</span>
-            </li>
-            <li className="opacity-50">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-base-content/50"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span className="line-through">Seamless cloud integration</span>
-            </li>
-            <li className="opacity-50">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-4 me-2 inline-block text-base-content/50"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
-              <span className="line-through">
-                Real-time collaboration tools
-              </span>
-            </li>
+      <div className="card max-w-96 mx-auto bg-base-100 rounded-2xl relative border border-[#f2f2f2FF] p-6 transition-all duration-300 hover:scale-[1.01] hover:shadow-xl">
+        {product.tag && (
+          <span
+            className={`badge badge-xs ${tagStyles[product.tagType].bg} ${tagStyles[product.tagType].text} rounded-full border-0 px-3 py-2 flex justify-center items-center absolute top-2 right-2`}
+          >
+            {product.tag}
+          </span>
+        )}
+        <div className="w-[60px] h-[60px] border border-gray-200 rounded-full flex justify-center items-center">
+          <span className="w-8 h-8 flex justify-center items-center">
+            {product.icon}
+          </span>
+        </div>
+        <div className="card-body p-0 mt-4">
+          <h2 className="text-2xl text-[#101727FF] font-bold">
+            {product.name}
+          </h2>
+          <p className="text-base text-[#627382FF]">{product.description}</p>
+          <p className="text-base text-[#627382FF]">
+            <span className="text-2xl font-bold text-[#101727FF]">
+              ${product.price}
+            </span>
+            /{product.period}
+          </p>
+          <ul className="flex flex-col gap-2 text-base font-medium text-[#627382FF]">
+            {product.features.map((feature, index) => {
+              return (
+                <li key={index}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span>{feature}</span>
+                </li>
+              );
+            })}
           </ul>
           <div className="mt-6">
-            <button className="btn btn-primary btn-block">Subscribe</button>
+            <button className="btn btn-block rounded-[100px]  bg-gradient-to-b from-[#4f39f6] to-[#9514fa] text-white px-4 py-3 font-bold border-none">
+              Buy Now
+            </button>
           </div>
         </div>
       </div>
